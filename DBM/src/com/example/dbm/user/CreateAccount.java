8ce374409409a152a39e10ecb155475a853f9216
vaka578 <eduardo.vazquez1@upr.edu>
2013-10-03 12:05:16 -0400

    Create funcionando y layout de user information

--- DBM/src/com/example/dbm/user/CreateAccount.java
+++ DBM/src/com/example/dbm/user/CreateAccount.java
@@ -3,18 +3,40 @@ package com.example.dbm.user;
 import com.example.dbm.R;
 import com.example.dbm.R.layout;
 import com.example.dbm.R.menu;
+import com.example.dbm.main.AfterLogIn;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccount extends Activity {
+
+
+	Button create;
+	Intent intent;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_account);
-		
+
+		create = (Button) findViewById(R.id.createAccount);
+
+
+		View.OnClickListener handler = new View.OnClickListener() {
+			public void onClick(View v){
+				intent = new Intent(CreateAccount.this,UserInformation.class);
+				startActivity(intent);
+
+			}
+		};
+
+		create.setOnClickListener(handler);
+
 	}
 
 	@Override
